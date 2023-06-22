6c358f772dbec1d454f73288986ef62e350c630e
Moiz <moizuddinr189@gmail.com>
2023-06-22 21:14:29 +0530

    modified

--- task12Practice.js
+++ task12Practice.js
@@ -1,3 +1,17 @@
+//Follow as youtuber Does
+
+//Promise.all
+// const promise1 = Promise.resolve('Hello WOrld');
+// const Promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>{
+//     setTimeout(resolve, 2000, 'Goodbye')
+// });
+
+// Promise.all([promise1,promise2,promise3]).then((values)=>{
+//     console.log(values);
+// })
+
+
 const user = [
     {title: 'POST1'},
      
